c128bfe7895ed182650fac7b114a1b17327652e0
Yashvardhan Jagnani <jagnani73@users.noreply.github.com>
2020-10-31 12:57:18 +0530

    feat(client): init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-import logo from "./logo.svg";
-import "./App.css";
+import Teacher from "./components/Teacher/Hero";
+import Student from "./components/Student/Hero";
+
 import "./tailwind.css";
 
-function App() {
+const App = () => {
+  const [isTeacher, setIsTeacher] = useState<boolean>(true);
+
+  const toggleIsTeacher = () => {
+    setIsTeacher(!isTeacher);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-3xl text-blue-400">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={toggleIsTeacher}>
+        change to {!isTeacher ? "teacher" : "student"}
+      </button>
+      {isTeacher ? <Teacher /> : <Student />}
+    </>
   );
-}
+};
 
 export default App;
