5f075defe847443781596466e9a654c121d5397f
Yashvardhan Jagnani <jagnani73@users.noreply.github.com>
2020-11-01 03:23:51 +0530

    chore(client): style

---
+++ client/src/components/Shared/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      className="hidden md:block w-full fixed bottom-0 left-0 text-center text-xl py-2"
+      style={{ backgroundColor: "#BEDCF0" }}
+    >
+      <p>
+        Made using 🧠 and ❤️ by <span className="font-bold">!!CODERS</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
